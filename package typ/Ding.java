3034dba8bb4c3ed9fc573d135648dfacfbb26622
Anikesh Chouhan <115870421+Anii2606@users.noreply.github.com>
2023-02-10 14:19:04 +0530

    Add files via upload

---
+++ package typ/Ding.java
@@ -0,0 +1,8 @@
+package garding;
+import garding.*;
+public class Ding{
+	public static void main(String[] args){
+		Grad markss =new Grad();
+		markss.ing();
+	}
+}
---
+++ package typ/bonus/Emp.java
@@ -0,0 +1,28 @@
+/*A company decided to give bonus of 5% to employee if his/her
+ year of service is more than 5 years.Ask user for their salary 
+ and year of service and print the net bonus amount.
+ Date:-22-01-23
+*/
+package bonus;
+import java.util.Scanner;
+public class Emp{
+	public void salry(){
+		System.out.println("Enter your salary and year(from howmany year are you working)");
+		int salary;
+		int year;
+		Scanner sc = new Scanner(System.in);
+		salary = sc.nextInt();
+		year = sc.nextInt();
+		if(year>=5){
+			System.out.println("your salary :"+(salary) + 
+			"\ncongratulation you got 5% bonus :"+ (salary*.05) +
+			"\ntotal salary :" + (salary + (salary*.05)));
+		//	System.out.println("Enter your year and salary");
+		}else {
+			System.out.println("your salary :"+ salary + "\nno bonus");
+			
+		}
+		
+	}
+	
+}
